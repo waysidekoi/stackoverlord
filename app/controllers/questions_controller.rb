69524cf5af2749176a15672e953f3cf0293c8719
dbc gogo <dbc@dbc.com>
2013-04-24 16:56:04 -0700

    modifed form for posting new question to use form_for and set up questions#create route

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,12 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    render :new
+    @question = Question.new
+  end
+
+  def create
+    current_user.questions.create(params[:question])
+
+    redirect_to root_path
   end
 end
