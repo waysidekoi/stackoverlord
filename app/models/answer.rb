37fd8eb28a427b07f09b9c0c4994f6f5db34c9c6
Eric Dykstra <eric@ericdykstra.me>
2013-04-24 21:04:22 -0700

    Working markdown comments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,8 +1,13 @@
 class Answer < ActiveRecord::Base
+  include MarkdownHelper
   attr_accessible :details, :user
 
   has_many :votes, :as => :votable
   belongs_to :user
 
   validates_presence_of :details
+
+  def md_details
+    md_render(details).html_safe
+  end
 end
